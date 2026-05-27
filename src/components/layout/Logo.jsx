c8c485b9ecf29f1cad5a2ab59a15d343e5702536
Carlos Eduardo <carloseduardomveloso@gmail.com>
2026-05-27 17:19:46 -0300

    enviando arquivos

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -1,28 +1,17 @@
 import { MapPin } from "lucide-react";
 
-function Logo({ variantText = "ocean", variantBg = "grandient-ocean" }) {
+function Logo({ variantText = "ocean", variantBg = "gradient-ocean", showText = true }) {
   const textColor = variantText === "sand" ? "text-sand" : "text-ocean";
-  const bgColor =
-    variantBg === "turquoise" ? "bg-turquoise" : "bg-gradient-ocean";
+  const bgColor = variantBg === "turquoise" ? "bg-turquoise" : "bg-gradient-ocean";
   return (
     <>
-      <h1
-        className={`
-          text-xl ${textColor}
-          font-black font-head
-          flex items-center gap-1.5`}
-      >
-        <span
-          className={`
-            inline-flex items-center justify-center
-               h-10 w-10
-               rounded-full
-               ${bgColor}
-            `}
-        >
+      <h1 className={`text-xl ${textColor} font-black font-head flex items-center gap-1.5`}>
+        <span className={`inline-flex items-center justify-center h-10 w-10 rounded-full ${bgColor}`}>
           <MapPin className="w-6 h-6 text-white" />
         </span>
-        <a href="">Farol Pitimbu</a>
+        <a href="" className={showText ? "hidden whitespace-nowrap sm:block" : "block"}>
+          Farol Pitimbu
+        </a>
       </h1>
     </>
   );
