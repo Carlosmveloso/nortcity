3a83a9801f74fd6068de0c81967efb3a40e3b1fd
Carlos Eduardo <carloseduardomveloso@gmail.com>
2026-06-29 21:10:24 -0300

    ajudatando a cor de fundo

--- src/components/home/FeaturedSection.jsx
+++ src/components/home/FeaturedSection.jsx
@@ -4,7 +4,7 @@ import FeaturedCard from '../ui/FeaturedCard';
 function FeaturedSection() {
     return (
         <>
-            <section className="bg-sand flex flex-col gap-10 items-center py-16">
+            <section className="bg-sand-dark flex flex-col gap-10 items-center py-16">
                 <section className="w-9/10">
                     <h2 className='mb-4 text-3xl font-head font-bold'>Destaques da semana</h2>
                     <p className='text-balance text-muted-foreground'>Os passeios e experiências mais procuradas pelos visitantes</p>
