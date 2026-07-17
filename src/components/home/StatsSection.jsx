41ea49224cf7d984d0720224e3b01222ff63ffe3
Carlos Eduardo <carloseduardomveloso@gmail.com>
2026-07-17 09:55:49 -0300

    Terminando seção de estatísticas

--- src/components/home/StatsSection.jsx
+++ src/components/home/StatsSection.jsx
@@ -1,7 +1,16 @@
 //* Números do projeto
+import StatsCard from '../ui/StatsCard';
 
 function StatsSection() {
-    return <></>;
+    return (
+        <>
+            <section className="bg-gradient-ocean py-16 md:py-24">
+                <section className="flex flex-col gap-8 lg:flex-row lg:justify-evenly lg:px-10">
+                    <StatsCard />
+                </section>
+            </section>
+        </>
+    );
 }
 
 export default StatsSection;
---
+++ src/data/statsSection.js
@@ -0,0 +1,22 @@
+import { Building2, Layers, RefreshCw } from "lucide-react";
+
+export const statsSection = [
+    {
+        icon: Building2,
+        total: "+ 75",
+        title: 'Negócios cadastrados',
+        description: 'Empresas e profissionais locais',
+    },
+    {
+        icon: Layers,
+        total: "+ 15",
+        title: 'Categorias',
+        description: 'Passeios, hospedagem, gastronimia e mais',
+    },
+    {
+        icon: RefreshCw,
+        total: 'Semanal',
+        title: 'Atualizado',
+        description: 'Informações sempre em dia',
+    },
+];
